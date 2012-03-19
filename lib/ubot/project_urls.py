aba8ac0cfe5e32b1f96890922e41a9aeb1d827aa
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2012-03-19 22:37:39 +0100

    Add a 'ubot instaweb' command to replace the example django project

--- lib/ubot/project_urls.py
+++ lib/ubot/project_urls.py
@@ -1,3 +1,5 @@
+# These is a full urlconf to be used by the instaweb helper
+
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.http import HttpResponseRedirect
@@ -6,12 +8,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # By default, go to the control interface
+    # Redirect '/' to the control interface
     (r'^$', lambda request: HttpResponseRedirect('control/')),
 
     # Authentication
     (r'^login/', 'django.contrib.auth.views.login'),
-
     (r'^logout/', 'django.contrib.auth.views.logout', {'next_page': "/"}),
 
     # Django admin documentation
@@ -22,9 +23,7 @@ urlpatterns = patterns('',
 
     # Ubot control interface
     (r'^control/', include('ubot.web.control.urls')),
-)
 
-urlpatterns += patterns('',
-  (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+    (r'^media/ubot/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
 )
